af4236a29d9a327b3db369165b8aa844491fea78
HimanshuHP <him.phogat123@gmail.com>
2020-10-21 22:42:56 +0530

    kubernetes script updated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,24 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=himanshuhp/ml-flask-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run ml-flask-api --image=$dockerpath:v1 --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
+while [ "$(kubectl describe pods ml-flask-api | grep ^Status: | head -1 | awk '{print $2}' | tr -d '\n')" != "Running" ]; do
+    echo "Waiting for POD status: $(kubectl describe pods ml-flask-api | grep ^Status: | head -1 | awk '{print $2}' | tr -d '\n')"
+done
+echo "POD status is Running"
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward ml-flask-api 8000:80
 
+kubectl logs ml-flask-api
