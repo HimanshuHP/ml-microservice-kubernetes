9b4bf61dd49f6218728ca1423bae23890b00d865
HimanshuHP <him.phogat123@gmail.com>
2020-10-21 19:36:04 +0530

    upload docker image to hub script updated

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=himanshuhp/ml-flask-api
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username himanshuhp
+docker image tag ml-flask-api $dockerpath:v1
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:v1
